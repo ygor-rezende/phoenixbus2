e6c0562beaabd3b45b642c1a5caa5679b7c44de8
ygor-rezende <71849255+ygor-rezende@users.noreply.github.com>
2023-09-09 12:59:56 -0400

    Fixed database port variable

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   host: process.env.HOST,
-  port: process.env.PORT,
+  port: process.env.DBPORT,
   database: process.env.DBNAME,
 });
 
