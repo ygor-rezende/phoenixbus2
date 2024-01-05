724869262f02244bafa4326b1e7e177f8fc33422
ygor-rezende <71849255+ygor-rezende@users.noreply.github.com>
2024-01-05 16:37:40 -0500

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,10 @@
-const allowedOrigins = ["http://localhost:3000", "http://www.yoursite.com"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://www.yoursite.com",
+  "100.20.92.101",
+  "44.225.181.72",
+  "44.227.217.144",
+  "https://phoenixintranet.onrender.com/",
+];
 
 module.exports = allowedOrigins;
