81f40f89ab0792d987e6cc07bb92126248c9d946
ygor-rezende <71849255+ygor-rezende@users.noreply.github.com>
2024-01-05 17:37:32 -0500

    fixed allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   "100.20.92.101",
   "44.225.181.72",
   "44.227.217.144",
-  "https://phoenixintranet.onrender.com/",
+  "https://phoenixintranet.onrender.com",
 ];
 
 module.exports = allowedOrigins;
--- server/middleware/credentials.js
+++ server/middleware/credentials.js
@@ -3,10 +3,7 @@ const allowedOrigins = require("../config/allowedOrigins");
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://phoenixintranet.onrender.com/"
-    );
+    res.header("Access-Control-Allow-Origin", origin);
 
     res.header(
       "Access-Control-Allow-Methods",
