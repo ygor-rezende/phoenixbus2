285abd7c17dc9ac07c9eed91ce00ac2b3d2be63c
ygor-rezende <71849255+ygor-rezende@users.noreply.github.com>
2024-03-12 19:16:17 -0400

    Fixed axios and created log clients trigger

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 let url = "";
-if (process.env.NODE_ENV === "development") url = process.env.LOCAL_SERVER_URL;
-else url = process.PROD_SERVER_URL;
-
+if (process.env.NODE_ENV === "development") url = "http://localhost:8000";
+else url = "https://app-aybje326pa-uc.a.run.app";
 
 export default axios.create({
   baseURL: url,
